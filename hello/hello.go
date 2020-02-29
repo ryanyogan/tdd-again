a33eaacbf2333ed554a3592e0c583404dee27358
ryanyogan <ryanyogan@gmail.com>
2020-02-29 14:03:21 -0800

    hello 2

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+const (
+	englishHelloPrefix = "Hello, "
+)
+
 // Hello - Returns "Hello, World"
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Ryan")
-	want := "Hello, Ryan"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got %q and wanted %q", got, want)
+		if got != want {
+			t.Errorf("got %q and wanted %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ryan")
+		want := "Hello, Ryan"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say Hello, World when arguments are empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
