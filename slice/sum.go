35813c019846df63ec560a409592040450cce390
ryanyogan <ryanyogan@gmail.com>
2020-03-01 20:34:48 -0800

    slice

--- slice/sum.go
+++ slice/sum.go
@@ -8,3 +8,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll -
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+// SumAllTails -
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:] // Slurp all after [n]index position
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
+}
